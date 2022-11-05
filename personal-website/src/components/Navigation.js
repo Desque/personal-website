89ed765b4bf49d6707b49956b88344d6d6bc7708
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-11-05 21:47:37 +0100

    create navigation

--- personal-website/src/components/Navigation.js
+++ personal-website/src/components/Navigation.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Navigation = () => {
     return (
         <div className="navigation">
-            
+            <NavLink to="/searching">Search</NavLink>
+            <NavLink to="/bio">Biography</NavLink>
+            <NavLink to="/soft-skills">Soft Skills</NavLink>
+            <NavLink to="/hard-skills">Hard Skills</NavLink>
+            <NavLink to="/projects">Projects</NavLink>
         </div>
     );
 };
