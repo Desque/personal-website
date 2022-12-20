1c0001d1aad74a94b4c6d4f8a9a8921c320af62a
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-12-20 23:23:55 +0100

    Refactoring navigation with tailwind

--- personal-website/src/components/Navigation.tsx
+++ personal-website/src/components/Navigation.tsx
@@ -14,22 +14,22 @@ interface NavigationProps {
 
 const MenuButtonStyle = (activePage : string, page : string) : string => {
     let globalStyle : string =
-        ' xl:text-xl lg:text-lg md:text-md sm:text-sm' +
-        ' xl:px-12 lg:px-8 md:px-4 sm:px-2' +
-        ' xl:py-6 lg:py-4 md:py-2' +
-        ' rounded-lg flex gap-4 justify-center items-center shadow-3xl'
+        ' lg:text-2xl text-md' +
+        ' rounded-lg flex flex-col justify-evenly items-center shadow-3xl' +
+        ' lg:m-6 sm:m-4 m-2' +
+        ' lg:w-48 sm:w-32 w-16 px-6 aspect-square'
     let specificStyle = activePage === page ? 'text-secondary bg-tertiary' : 'text-primary bg-white hover:text-tertiary'
     return specificStyle + globalStyle
 }
 
 const Navigation = (props : NavigationProps) => {
     return (
-        <div className="xl:m-16 lg:m-8 md:m-4 sm:m-2 w-100 flex justify-evenly">
-            <NavLink to="/searching" className={MenuButtonStyle(props.activePage,'searching')}><BsSearch className="align-icon"/><p className="btn-description">Search</p></NavLink>
-            <NavLink to="/bio" className={MenuButtonStyle(props.activePage,'biography')}><BsFillPersonLinesFill className="align-icon"/><p className="btn-description">Biography</p></NavLink>
-            <NavLink to="/soft-skills" className={MenuButtonStyle(props.activePage,'soft-skills')}><BsPeopleFill className="align-icon"/><p className="btn-description">Soft Skills</p></NavLink>
-            <NavLink to="/hard-skills" className={MenuButtonStyle(props.activePage,'hard-skills')}><BsTerminalFill className="align-icon"/><p className="btn-description">Hard Skills</p></NavLink>
-            <NavLink to="/projects" className={MenuButtonStyle(props.activePage,'projects')}><BsFillCollectionFill className="align-icon"/><p className="btn-description">Projects</p></NavLink>
+        <div className="xl:m-16 lg:m-8 md:m-4 sm:m-2 w-100 flex justify-evenly flex-wrap">
+            <NavLink to="/searching" className={MenuButtonStyle(props.activePage,'searching')}><BsSearch className="align-middle"/><p className="hidden sm:block whitespace-nowrap">Search</p></NavLink>
+            <NavLink to="/bio" className={MenuButtonStyle(props.activePage,'biography')}><BsFillPersonLinesFill className="align-middle"/><p className="hidden sm:block whitespace-nowrap">Biography</p></NavLink>
+            <NavLink to="/soft-skills" className={MenuButtonStyle(props.activePage,'soft-skills')}><BsPeopleFill className="align-middle"/><p className="hidden sm:block whitespace-nowrap">Soft Skills</p></NavLink>
+            <NavLink to="/hard-skills" className={MenuButtonStyle(props.activePage,'hard-skills')}><BsTerminalFill className="align-middle"/><p className="hidden sm:block whitespace-nowrap">Hard Skills</p></NavLink>
+            <NavLink to="/projects" className={MenuButtonStyle(props.activePage,'projects')}><BsFillCollectionFill className="align-middle"/><p className="hidden sm:block whitespace-nowrap">Projects</p></NavLink>
         </div>
     );
 };
